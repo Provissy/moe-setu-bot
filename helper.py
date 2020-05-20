621567fc286cba574728f2c7d3c1c89362092be1
Provissy <provissy@gmail.com>
2020-05-20 20:05:58 +0900

    update help msg

--- helper.py
+++ helper.py
@@ -1,3 +1,6 @@
+import shlex
+
+
 def get_help_msg(helpType:str):
     if helpType == "help":
         return """
@@ -6,14 +9,15 @@ moe-setu-bot by Plow. Released under the GPLv3 License.
 Comes with ABSOLUTELY NO WARRANTY. All rights reserved. 
     
 Usage: /<command> <flags>
+Note: '/' is omittable for convenience
 
 Available commands:
- /identify         Identify an image and query for its details.
- /upscale          Upscale an image using waifu2x CuNet model.
- /pixiv            Query or download Pixiv resources, see also /pixiv help.
- /echo             Echo what you have sent.
- /help             Print this help message.
- /help cn          輸出中文幫助訊息。
+ /identify       Identify an image and query for its details.
+ /upscale        Upscale an image using waifu2x CuNet model.
+ /pixiv          Query or download Pixiv resources, see also /pixiv help.
+ /echo           Echo what you have sent.
+ /help           Print this help message.
+ /help cn        輸出中文幫助訊息。
  ```
     """
 
@@ -24,13 +28,14 @@ moe-setu-bot by Plow. Released under the GPLv3 License.
 Comes with ABSOLUTELY NO WARRANTY. All rights reserved. 
     
 使用方法: /<指令> <參數>
+提示: '/'為方便起見可以忽略
 
 可用指令:
- /identify         識別圖片並搜索詳情。
- /upscale          使用waifu2x CuNet放大圖片。
- /pixiv            檢索或下載Pixiv各種資源，詳見/pixiv help。
- /echo             復讀。
- /help cn          輸出此段幫助訊息。
+ /identify       識別圖片並搜尋詳情。
+ /upscale        使用waifu2x CuNet放大圖片。
+ /pixiv          檢索或下載Pixiv各種資源，詳見/pixiv help。
+ /echo           復讀。
+ /help cn        輸出此段幫助訊息。
  ```
     """
 
@@ -42,14 +47,21 @@ Comes with ABSOLUTELY NO WARRANTY. All rights reserved.
 
 /pixiv <sub-command> <args> <flags>
 
-Usage: 
-pull            Download specific resource from pixiv.
-    id          Illustration ID
-    artist-id   Artist ID
-query           Query for a specific resource.
-    id          Illustration ID
-ranking <r18>   Get illustrations from Pixiv rankings.
-    daily       Daily ranking.
-    weekly      Weekly ranking.
+Sub-commands: 
+ pull          Download specific resource from pixiv.
+   id          Illustration ID
+   artist-id   Artist ID
+ query         Query for a specific resource.
+   id          Illustration ID
+ ranking <r18> Get illustrations from Pixiv rankings.
+   daily       Daily ranking.
+   weekly      Weekly ranking.
 ```
         """
+
+
+def parsePixivArgs(str_args):
+    splitted_arguments = shlex.split(str_args)
+    # Remove first and second command
+    splitted_arguments.pop(0)
+    return splitted_arguments
