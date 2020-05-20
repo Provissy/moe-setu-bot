9bf84708e3bddf7bd18bd0deb555e99307e54ad9
Provissy <provissy@gmail.com>
2020-05-20 20:05:03 +0900

    Create state_control.py

---
+++ state_control.py
@@ -0,0 +1,11 @@
+class StateControl:
+    list_user_waiting_identify = []
+
+
+class GlobalConst:
+    TOKEN= ""
+    SAUCENAO_API = ""
+    SAUCENAO_URL = ""
+    PIXIV_USER = ""
+    PIXIV_PASS = ""
+    IMG_SERVER_ADDR = ""
